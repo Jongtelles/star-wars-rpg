2304813b19b41beeeaa908352658b711706c469d
jongtelles <jongtelles@gmail.com>
2018-02-10 16:43:55 -0500

    Major updates to javacsript, html, and css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,61 +2,138 @@ $(document).ready(function () {
     // setting initial variables
     var charOne = {
         name: "Luke",
-        counterdmg: 5,
+        counterDmg: 5,
         maxHp: 100,
         currentDmg: undefined,
         initialDmg: 10,
         currentHp: undefined,
         isAttacker: false,
         isDefender: false,
-        potentialEnemy: false,
     }
     var charTwo = {
         name: "Han",
-        counterdmg: 5,
+        counterDmg: 5,
         maxHp: 100,
         currentDmg: undefined,
         initialDmg: 10,
         currentHp: undefined,
         isAttacker: false,
         isDefender: false,
-        potentialEnemy: false,
     }
     var charThree = {
-        name: "Finn",
-        counterdmg: 5,
+        name: "Leia",
+        counterDmg: 5,
         maxHp: 100,
         currentDmg: undefined,
         initialDmg: 10,
         currentHp: undefined,
         isAttacker: false,
         isDefender: false,
-        potentialEnemy: false,
     }
     var charFour = {
-        name: "Poe",
-        counterdmg: 5,
+        name: "Darth",
+        counterDmg: 5,
         maxHp: 100,
         currentDmg: undefined,
         initialDmg: 10,
         currentHp: undefined,
         isAttacker: false,
         isDefender: false,
-        potentialEnemy: false,
     };
-    var enemiesRemaining;
-
-    //function that resets inital character select positions, clears the battleArena
-
-    // function sets characters back to default variables
+    var enemiesRemaining, mainChar, enemy;
+    var characterSelected = false;
+    var enemySelected = false;
+    //function that resets inital character select positions, clears the battleArena, resets character object values with the charResetter function
+    function gameReset() {
+        enemiesRemaining = undefined;
+        mainChar = undefined;
+        characterSelected = false;
+        $("#attacker").empty();
+        $("#defender").empty();
+        $(".character").show();
+        charResetter(charOne, charTwo, charThree, charFour);
+    };
+    // functions adjust character object variables
     function charResetter(charGoesHere) {
         charGoesHere.currentHp = undefined;
         charGoesHere.currentDmg = undefined;
         charGoesHere.isAttacker = false;
         charGoesHere.isDefender = false;
-        charGoesHere.potentialEnemy = false;
+        enemySelected = false;
     };
-    // when a character is moved to battlearena, set their currentHp to their maxHp, and currentDmg to initialDmg, when a character dies, remove them from battlearena
-    function battleArena() {};
 
+    function enemyCharSetup(charGoesHere) {
+        charGoesHere.currentHp = charGoesHere.maxHp;
+        charGoesHere.currentDmg = charGoesHere.counterDmg;
+        charGoesHere.isDefender = true;
+        enemySelected = true;
+    };
+
+    function mainCharSetup(charGoesHere) {
+        charGoesHere.currentHp = charGoesHere.maxHp;
+        charGoesHere.currentDmg = charGoesHere.initialDmg;
+        charGoesHere.isAttacker = true;
+        characterSelected = true;
+    };
+    $(".character").on("click", function () {
+        if (characterSelected === true && enemySelected === false && ($(this).attr("id") === "luke")) {
+            $(this).toggle();
+            $("#defender").html('<img src="./assets/images/luke.jpg" style="height: 300px">');
+            enemy = charOne;
+            enemyCharSetup(enemy);
+            $("h2.message").text("Defeat current enemey before selecting new enemy");
+        } else if (characterSelected === false && enemySelected === false && ($(this).attr("id") === "luke")) {
+            $(this).toggle();
+            $("#attacker").html('<img src="./assets/images/luke.jpg" style="height: 300px">');
+            mainChar = charOne;
+            mainCharSetup(mainChar);
+            $("h2.message").text("Select enemy");
+            $("#versus").html('<img src="./assets/images/versus.png" style="height: 150px">');
+        }
+        if (characterSelected === true && enemySelected === false && ($(this).attr("id") === "han")) {
+            $(this).toggle();
+            $("#defender").html('<img src="./assets/images/han.jpg" style="height: 300px">');
+            enemy = charTwo;
+            enemyCharSetup(enemy);
+            $("h2.message").text("Defeat current enemey before selecting new enemy");
+        } else if (characterSelected === false && enemySelected === false && ($(this).attr("id") === "han")) {
+            $(this).toggle();
+            $("#attacker").html('<img src="./assets/images/han.jpg" style="height: 300px">');
+            mainChar = charTwo;
+            mainCharSetup(mainChar);
+            $("h2.message").text("Select enemy");
+            $("#versus").html('<img src="./assets/images/versus.png" style="height: 150px">');
+        }
+        if (characterSelected === true && enemySelected === false && ($(this).attr("id") === "leia")) {
+            $(this).toggle();
+            $("#defender").html('<img src="./assets/images/leia.jpg" style="height: 300px">');
+            enemy = charThree;
+            enemyCharSetup(enemy);
+            $("h2.message").text("Defeat current enemey before selecting new enemy");
+        } else if (characterSelected === false && enemySelected === false && ($(this).attr("id") === "leia")) {
+            $(this).toggle();
+            $("#attacker").html('<img src="./assets/images/leia.jpg" style="height: 300px">');
+            mainChar = charThree;
+            mainCharSetup(mainChar);
+            $("h2.message").text("Select enemy");
+            $("#versus").html('<img src="./assets/images/versus.png" style="height: 150px">');
+        }
+        if (characterSelected === true && enemySelected === false && ($(this).attr("id") === "darth")) {
+            $(this).toggle();
+            $("#defender").html('<img src="./assets/images/darth.jpg" style="height: 300px">');
+            enemy = charFour;
+            enemyCharSetup(enemy);
+            $("h2.message").text("Defeat current enemey before selecting new enemy");
+        } else if (characterSelected === false && enemySelected === false && ($(this).attr("id") === "darth")) {
+            $(this).toggle();
+            $("#attacker").html('<img src="./assets/images/darth.jpg" style="height: 300px">');
+            mainChar = charFour;
+            mainCharSetup(mainChar);
+            $("h2.message").text("Select enemy");
+            $("#versus").html('<img src="./assets/images/versus.png" style="height: 150px">');
+        }
+    });
 });
+//if (enemy.currentHp = 0){enemiesRemaining--; charResetter(enemy); $("h2.message").text("Select enemy");}
+//if (enemiesRemaining = 0){} game ends in win
+//if (mainChar.currentHp <= 0){} game ends in loss
